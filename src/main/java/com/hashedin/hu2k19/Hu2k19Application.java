ceb890a382c58a9f1fed26420ce29bf468ca2071
Husain Topiwala <husaint21@gmail.com>
2019-07-27 23:59:38 +0530

    project scaffolding

---
+++ src/main/java/com/hashedin/hu2k19/Hu2k19Application.java
@@ -0,0 +1,13 @@
+package com.hashedin.hu2k19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hu2k19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hu2k19Application.class, args);
+	}
+
+}
